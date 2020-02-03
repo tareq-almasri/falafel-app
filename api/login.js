c667b9e40581474aaf4357ff1d522c9461aa772a
tareq-almasri <talmasreq@gmail.com>
2020-02-03 21:38:21 +0100

    .

--- api/login.js
+++ api/login.js
@@ -22,16 +22,15 @@ mongoose.connect(
 
 module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  User.findOne({ username: 'ion' })
-
+  User.findOne({ username: "ion" })
     .then(user => {
       // user with this email not found? => error
       if (!user) {
-        res.status(400).json( "invalid username or password" );
+        res.status(400).json("invalid username or password");
       }
 
       // compare passwords using bcrypt.compare() function
-      bcrypt.compare('123456789', user.password).then(success => {
+      bcrypt.compare("123456789", user.password).then(success => {
         // user password does not match password from login form? => error
         if (!success) {
           res.status(400).json({ err: "invalid username or password" });
@@ -39,13 +38,13 @@ module.exports = (req, res) => {
         // create JWT token by signing
         let secret = "top-secret";
         let token = jwt.sign(
-          { aud: "iPhone-App", username: user.username }, // WHAT data to sign
+          { aud: "iPhone-App", username: user.username, id: user._id }, // WHAT data to sign
           secret //, // signing key
           // { expiresIn: "1h" } // expiry time
         );
 
         // return token
-        res.json({token}); // => same as: { "token": token }
+        res.json({ token }); // => same as: { "token": token }
       });
     })
     .catch(err => res.status(400).json("err: " + err));
--- components/Login.js
+++ components/Login.js
@@ -61,7 +61,7 @@ class Login extends Component {
   handleDone = () => {
     if (this.state.username && this.state.password) {
       fetch(
-        `http://falafel-server-i8xqvqfts.now.sh/api/login/?username=catjjjnl&password=helloo`
+        `http://falafel-server-52xg8gztj.now.sh/api/login/?username=catjjjnl&password=helloo`
       )
         .then(res => res.json())
         .then(data => console.log(data));
