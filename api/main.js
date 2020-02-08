4ce163e1382db3fd76884c5410ef8e7b60cfdb3e
tareq-almasri <talmasreq@gmail.com>
2020-02-08 14:05:32 +0100

    .

--- api/main.js
+++ api/main.js
@@ -26,7 +26,7 @@ module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   let user = jwt.verify(req.query.token, process.env.ACCESS_TOKEN_SECRET);
 
-  res.send({wtf: user})
+  res.send({user})
 
   // User.find({ username: user.username })
   //   .then(userFound => {
--- components/Main.js
+++ components/Main.js
@@ -13,33 +13,33 @@ import {
 import { ACCESS_SERVER_URL } from "react-native-dotenv";
 
 class Main extends Component {
-  state = {
-    username: '',
-    password: '',
-    tdee: 0,
-    goalCal: 0,
-    diet: '',
-    proteinDL: 0,
-    carbsDL: 0,
-    fatDL: 0,
-    dailyPlan: {
-      wakeUp: '',
-      breakfast: '',
-      lunch: '',
-      dinner: '',
-      sleep: ''
-    },
-    sport: [],
-    udi: {
-      calCount: 0,
-      proteinCount: 0,
-      fatCount: 0,
-      carbsCount: 0,
-      waterCount: 0,
-      caffCount: 0,
-      sugarCount: 0
-    }
-  };
+  // state = {
+  //   username: '',
+  //   password: '',
+  //   tdee: 0,
+  //   goalCal: 0,
+  //   diet: '',
+  //   proteinDL: 0,
+  //   carbsDL: 0,
+  //   fatDL: 0,
+  //   dailyPlan: {
+  //     wakeUp: '',
+  //     breakfast: '',
+  //     lunch: '',
+  //     dinner: '',
+  //     sleep: ''
+  //   },
+  //   sport: [],
+  //   udi: {
+  //     calCount: 0,
+  //     proteinCount: 0,
+  //     fatCount: 0,
+  //     carbsCount: 0,
+  //     waterCount: 0,
+  //     caffCount: 0,
+  //     sugarCount: 0
+  //   }
+  // };
 
   gimme=()=> {
     let token=this.props.navigation.getParam("token");
