f26cf3e09d10c8b39f9ffe4f81e7aed1d98bb87b
tareq-almasri <talmasreq@gmail.com>
2020-02-08 14:02:36 +0100

    .

--- api/main.js
+++ api/main.js
@@ -26,7 +26,7 @@ module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   let user = jwt.verify(req.query.token, process.env.ACCESS_TOKEN_SECRET);
 
-  res.send({name: user.username})
+  res.send({wtf: user})
 
   // User.find({ username: user.username })
   //   .then(userFound => {
